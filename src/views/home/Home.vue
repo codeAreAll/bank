9520525fb151622c00e4d24d09f5cdf5d7da0ef7
xuebawudi <812683139@qq.com>
2021-06-01 20:34:17 +0800

    bank管理

---
+++ src/views/home/Home.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="home">
+   <nav-bar class="home-nav"><div slot="center">首页</div></nav-bar>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import NavBar from '@/components/common/navbar/NavBar'
+
+export default {
+  name: 'Home',
+  components: {
+    NavBar
+  }
+}
+</script>
+<style scoped>
+ .home-nav{
+   background: #1979F8;
+   color:#fff;
+   font-size: 18px;
+   height: 44px;
+ }
+</style>
